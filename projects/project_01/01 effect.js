e6437237081dfb396627cf1ccca6d6a80bb10036
PickleRick-C131 <Harlanlaw@163.com>
2023-05-04 12:06:29 -0400

    Update 01 effect.js

--- projects/project_01/01 effect.js
+++ projects/project_01/01 effect.js
@@ -5,16 +5,23 @@ const images = container.querySelectorAll('img');
 
 
 images.forEach((img) => {
-    // 随机计算出left和top值
-    const left = Math.floor(Math.random() * container.offsetWidth);
-    const top = Math.floor(Math.random() * container.offsetHeight);
-    
-    // 随机计算出旋转角度
-    const rotate = Math.floor(Math.random() * 360);
+  const containerRect = container.getBoundingClientRect();
+  const containerLeft = containerRect.left;
+  const containerTop = containerRect.top;
+  const containerRight = containerLeft + container.offsetWidth;
+  const containerBottom = containerTop + container.offsetHeight;
   
-    // 设置图片的定位、位置和旋转角度
-    img.style.position = 'absolute';
-    img.style.left = left + 'px';
-    img.style.top = top + 'px';
-    img.style.transform = `rotate(${rotate}deg)`;
-  });
+  // 随机计算出left和top值，限制在容器元素内
+  const left = Math.max(containerLeft, Math.min(containerRight, containerLeft + Math.floor(Math.random() * container.offsetWidth)));
+  const top = Math.max(containerTop, Math.min(containerBottom, containerTop + Math.floor(Math.random() * container.offsetHeight)));
+  
+  // 随机计算出旋转角度
+  const rotate = Math.floor(Math.random() * 60) - 30;
+
+  // 设置图片的定位、位置和旋转角度
+  img.style.position = 'absolute';
+  img.style.left = left + 'px';
+  img.style.top = top + 'px';
+  img.style.transform = `rotate(${rotate}deg)`;
+});
+  
