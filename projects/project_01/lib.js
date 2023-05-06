500c480b46495707982de158e84384ebb5fa5e6a
PickleRick-C131 <Harlanlaw@163.com>
2023-05-05 21:12:16 -0400

    1

--- projects/project_01/lib.js
+++ projects/project_01/lib.js
@@ -1,5 +1,10 @@
 
 
+
+
+
+
+
 //USD100
 const img = document.querySelector('.USD100');
 const tooltip = document.querySelector('.text_USD100');
@@ -75,4 +80,302 @@ img_USD5.addEventListener('mousemove', function(e) {
 img_USD5.addEventListener('mouseout', function() {
   tooltip_USD5.style.display = 'none';
 });
-//USD10
+//USD5
+
+
+
+
+
+//lira1
+const img_lira1 = document.querySelector('.lira1');
+const tooltip_lira1 = document.querySelector('.text_lira1');
+
+img_lira1.addEventListener('mousemove', function(e) {
+  tooltip_lira1.style.display = 'block';
+  tooltip_lira1.style.top = e.pageY + 'px';
+  tooltip_lira1.style.left = (e.pageX + 20) + 'px';
+});
+
+img_lira1.addEventListener('mouseout', function() {
+  tooltip_lira1.style.display = 'none';
+});
+//lira1
+
+//lira100
+const img_lira100 = document.querySelector('.lira100');
+const tooltip_lira100 = document.querySelector('.text_lira100');
+
+img_lira100.addEventListener('mousemove', function(e) {
+  tooltip_lira100.style.display = 'block';
+  tooltip_lira100.style.top = e.pageY + 'px';
+  tooltip_lira100.style.left = (e.pageX + 20) + 'px';
+});
+
+img_lira100.addEventListener('mouseout', function() {
+  tooltip_lira100.style.display = 'none';
+});
+//lira100
+
+
+//lira10
+const img_lira10 = document.querySelector('.lira10');
+const tooltip_lira10 = document.querySelector('.text_lira10');
+
+img_lira10.addEventListener('mousemove', function(e) {
+  tooltip_lira10.style.display = 'block';
+  tooltip_lira10.style.top = e.pageY + 'px';
+  tooltip_lira10.style.left = (e.pageX + 20) + 'px';
+});
+
+img_lira10.addEventListener('mouseout', function() {
+  tooltip_lira10.style.display = 'none';
+});
+//lira10
+
+
+//lira200
+const img_lira200 = document.querySelector('.lira200');
+const tooltip_lira200 = document.querySelector('.text_lira200');
+
+img_lira200.addEventListener('mousemove', function(e) {
+  tooltip_lira200.style.display = 'block';
+  tooltip_lira200.style.top = e.pageY + 'px';
+  tooltip_lira200.style.left = (e.pageX + 20) + 'px';
+});
+
+img_lira200.addEventListener('mouseout', function() {
+  tooltip_lira200.style.display = 'none';
+});
+//lira200
+
+
+//lira20
+const img_lira20 = document.querySelector('.lira20');
+const tooltip_lira20 = document.querySelector('.text_lira20');
+
+img_lira20.addEventListener('mousemove', function(e) {
+  tooltip_lira20.style.display = 'block';
+  tooltip_lira20.style.top = e.pageY + 'px';
+  tooltip_lira20.style.left = (e.pageX + 20) + 'px';
+});
+
+img_lira20.addEventListener('mouseout', function() {
+  tooltip_lira20.style.display = 'none';
+});
+//lira20
+
+
+//lira5
+const img_lira5 = document.querySelector('.lira5');
+const tooltip_lira5 = document.querySelector('.text_lira5');
+
+img_lira5.addEventListener('mousemove', function(e) {
+  tooltip_lira5.style.display = 'block';
+  tooltip_lira5.style.top = e.pageY + 'px';
+  tooltip_lira5.style.left = (e.pageX - 600) + 'px';
+});
+
+img_lira5.addEventListener('mouseout', function() {
+  tooltip_lira5.style.display = 'none';
+});
+//lira5
+
+
+//lira50
+const img_lira50 = document.querySelector('.lira50');
+const tooltip_lira50 = document.querySelector('.text_lira50');
+
+img_lira50.addEventListener('mousemove', function(e) {
+  tooltip_lira50.style.display = 'block';
+  tooltip_lira50.style.top = e.pageY + 'px';
+  tooltip_lira50.style.left = (e.pageX - 600) + 'px';
+});
+
+img_lira50.addEventListener('mouseout', function() {
+  tooltip_lira50.style.display = 'none';
+});
+//lira50
+
+
+
+
+
+
+//mark10
+const img_mark10 = document.querySelector('.mark10');
+const tooltip_mark10 = document.querySelector('.text_mark10');
+
+img_mark10.addEventListener('mousemove', function(e) {
+  tooltip_mark10.style.display = 'block';
+  tooltip_mark10.style.top = e.pageY + 'px';
+  tooltip_mark10.style.left = (e.pageX + 20) + 'px';
+});
+
+img_mark10.addEventListener('mouseout', function() {
+  tooltip_mark10.style.display = 'none';
+});
+//mark10
+
+
+//mark100
+const img_mark100 = document.querySelector('.mark100');
+const tooltip_mark100 = document.querySelector('.text_mark100');
+
+img_mark100.addEventListener('mousemove', function(e) {
+  tooltip_mark100.style.display = 'block';
+  tooltip_mark100.style.top = e.pageY + 'px';
+  tooltip_mark100.style.left = (e.pageX + 20) + 'px';
+});
+
+img_mark100.addEventListener('mouseout', function() {
+  tooltip_mark100.style.display = 'none';
+});
+//mark100
+
+
+//mark20
+const img_mark20 = document.querySelector('.mark20');
+const tooltip_mark20 = document.querySelector('.text_mark20');
+
+img_mark20.addEventListener('mousemove', function(e) {
+  tooltip_mark20.style.display = 'block';
+  tooltip_mark20.style.top = e.pageY + 'px';
+  tooltip_mark20.style.left = (e.pageX + 20) + 'px';
+});
+
+img_mark20.addEventListener('mouseout', function() {
+  tooltip_mark20.style.display = 'none';
+});
+//mark100
+
+
+//mark200
+const img_mark200 = document.querySelector('.mark200');
+const tooltip_mark200 = document.querySelector('.text_mark200');
+
+img_mark200.addEventListener('mousemove', function(e) {
+  tooltip_mark200.style.display = 'block';
+  tooltip_mark200.style.top = e.pageY + 'px';
+  tooltip_mark200.style.left = (e.pageX + 20) + 'px';
+});
+
+img_mark200.addEventListener('mouseout', function() {
+  tooltip_mark200.style.display = 'none';
+});
+//mark100
+
+
+//mark5
+const img_mark5 = document.querySelector('.mark5');
+const tooltip_mark5 = document.querySelector('.text_mark5');
+
+img_mark5.addEventListener('mousemove', function(e) {
+  tooltip_mark5.style.display = 'block';
+  tooltip_mark5.style.top = e.pageY + 'px';
+  tooltip_mark5.style.left = (e.pageX + 20) + 'px';
+});
+
+img_mark5.addEventListener('mouseout', function() {
+  tooltip_mark5.style.display = 'none';
+});
+//mark5
+
+
+//mark50
+const img_mark50 = document.querySelector('.mark50');
+const tooltip_mark50 = document.querySelector('.text_mark50');
+
+img_mark50.addEventListener('mousemove', function(e) {
+  tooltip_mark50.style.display = 'block';
+  tooltip_mark50.style.top = e.pageY + 'px';
+  tooltip_mark50.style.left = (e.pageX - 600) + 'px';
+});
+
+img_mark50.addEventListener('mouseout', function() {
+  tooltip_mark50.style.display = 'none';
+});
+//mark50
+
+
+//mark500
+const img_mark500 = document.querySelector('.mark500');
+const tooltip_mark500 = document.querySelector('.text_mark500');
+
+img_mark500.addEventListener('mousemove', function(e) {
+  tooltip_mark500.style.display = 'block';
+  tooltip_mark500.style.top = e.pageY + 'px';
+  tooltip_mark500.style.left = (e.pageX - 600) + 'px';
+});
+
+img_mark500.addEventListener('mouseout', function() {
+  tooltip_mark500.style.display = 'none';
+});
+//mark500
+
+
+
+
+
+
+//pound1
+const img_pound1 = document.querySelector('.pound1');
+const tooltip_pound1 = document.querySelector('.text_pound1');
+
+img_pound1.addEventListener('mousemove', function(e) {
+  tooltip_pound1.style.display = 'block';
+  tooltip_pound1.style.top = e.pageY + 'px';
+  tooltip_pound1.style.left = (e.pageX + 20) + 'px';
+});
+
+img_pound1.addEventListener('mouseout', function() {
+  tooltip_pound1.style.display = 'none';
+});
+//pound1
+
+
+//pound20
+const img_pound20 = document.querySelector('.pound20');
+const tooltip_pound20 = document.querySelector('.text_pound20');
+
+img_pound20.addEventListener('mousemove', function(e) {
+  tooltip_pound20.style.display = 'block';
+  tooltip_pound20.style.top = e.pageY + 'px';
+  tooltip_pound20.style.left = (e.pageX + 20) + 'px';
+});
+
+img_pound20.addEventListener('mouseout', function() {
+  tooltip_pound20.style.display = 'none';
+});
+//pound20
+
+
+//pound50
+const img_pound50 = document.querySelector('.pound50');
+const tooltip_pound50 = document.querySelector('.text_pound50');
+
+img_pound50.addEventListener('mousemove', function(e) {
+  tooltip_pound50.style.display = 'block';
+  tooltip_pound50.style.top = e.pageY + 'px';
+  tooltip_pound50.style.left = (e.pageX - 600) + 'px';
+});
+
+img_pound50.addEventListener('mouseout', function() {
+  tooltip_pound50.style.display = 'none';
+});
+//pound50
+
+
+//pound5
+const img_pound5 = document.querySelector('.pound5');
+const tooltip_pound5 = document.querySelector('.text_pound5');
+
+img_pound5.addEventListener('mousemove', function(e) {
+  tooltip_pound5.style.display = 'block';
+  tooltip_pound5.style.top = e.pageY + 'px';
+  tooltip_pound5.style.left = (e.pageX - 600) + 'px';
+});
+
+img_pound5.addEventListener('mouseout', function() {
+  tooltip_pound5.style.display = 'none';
+});
+//pound5
